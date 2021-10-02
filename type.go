05a6d456e6dfada209fc924105afa7e00ff09056
m4rshmallow <prhdko.d.m@gmail.com>
2021-10-03 01:16:57 +0300

    save

--- type.go
+++ type.go
@@ -2,7 +2,6 @@ package go_envs
 
 import (
 	"fmt"
-	"os"
 )
 
 type Env map[string]string
@@ -10,8 +9,7 @@ type Env map[string]string
 func (e Env) Get(n string) string {
 	value, ok := e[n]
 	if !ok {
-		notifyKO(fmt.Sprintf(" %s doesn't exists\n", n))
-		os.Exit(1)
+		panic(fmt.Sprintf("%s doesn't exists\n", n))
 	}
 
 	return value
